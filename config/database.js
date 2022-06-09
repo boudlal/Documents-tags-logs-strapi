46645fd0701f886b518ca7966eed44748d6e2a7c
Boudlal <boudlal98@gmail.com>
2022-06-09 23:35:16 +0100

    initialisation du projet + configuration de MySQL database

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = ({ env }) => {
+  console.log('----env', env);
+  return {
+    connection: {
+      client: 'mysql',
+      connection: {
+        host: env("DB_HOST"),
+        database: env("DB_NAME"),
+        user: env("DB_USER"),
+        password: env("DB_PASSWORD")
+      },
+      useNullAsDefault: true,
+    },
+  }
+}
